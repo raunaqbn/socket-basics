87868ee6a2a1377f5074494e309306d628ad0d47
Raunaq Naidu <raunaq.naidu@gmail.com>
2016-02-16 19:07:13 -0800

    Adding forms and submit actions

--- public/js/app.js
+++ public/js/app.js
@@ -7,4 +7,18 @@ socket.on('connect',function (){
 socket.on('message', function (message){
 	console.log('New message');
 	console.log(message.text);
-})
+});
+
+
+//Handles submitting of new message
+
+var $form = jQuery('#message-form');
+
+$form.on('submit',function  (event) {
+	event.preventDefault();
+	var $message = $form.find('input[name=message]');
+	socket.emit('message',{
+		text: $message.val()
+	})	
+	$message.val('');
+});
