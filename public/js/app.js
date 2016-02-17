905b3c080527b553bc4953a01d0b22f31315998c
Raunaq Naidu <raunaq.naidu@gmail.com>
2016-02-16 17:41:56 -0800

    Added custom message event

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,8 @@ var socket = io();
 socket.on('connect',function (){
 	console.log('Connection made successfully to the server');
 });
+
+socket.on('message', function (message){
+	console.log('New message');
+	console.log(message.text);
+})
